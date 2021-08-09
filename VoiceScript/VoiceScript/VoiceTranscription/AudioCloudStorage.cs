825d870fe009603042d03397df7fa2fc34a41480
Kristýna Lhoťanová <kristyna.posta@seznam.cz>
2021-08-09 15:22:27 +0200

    Create AudioCloudStorage skeleton for possible Cloud Storage extension

---
+++ VoiceScript/VoiceScript/VoiceTranscription/AudioCloudStorage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Google.Cloud.Storage.V1;
+
+namespace VoiceScript.VoiceTranscription
+{
+    class AudioCloudStorage
+    {
+        readonly string projectId = "xxx";
+        readonly string filePath;
+        readonly string contentType;
+        readonly string bucketName;
+        readonly StorageClient client;
+
+        public AudioCloudStorage(string filename, string fileContentType)
+        {
+            filePath = filename;
+            contentType = fileContentType;
+
+            #region InitializeProjectBucket
+            client = StorageClient.Create();
+            bucketName = Guid.NewGuid().ToString();
+            client.CreateBucket(projectId, bucketName);
+            #endregion
+        }
+
+        public string BucketName => bucketName;
+        public string FileStorageUri => $"gs://{bucketName}/{filePath}";
+
+        public void Upload(byte[] content)
+        {
+            client.UploadObject(bucketName, filePath, contentType, new MemoryStream(content));
+        }
+
+        public void Download(string objectDownloadName = null)
+        {
+            objectDownloadName ??= filePath;
+
+            using var stream = File.OpenWrite(objectDownloadName);
+            client.DownloadObject(bucketName, filePath, stream);
+        }
+    }
+}
