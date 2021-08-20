6e36f9607e2d6b4c6c226c0dfb3ff47d7d60a06b
Kristýna Lhoťanová <kristyna.posta@seznam.cz>
2021-08-20 22:55:18 +0200

    Change decomposition of EditCommand.ProcessCommand method

--- VoiceScript/VoiceScript/DiagramModel/Commands/EditCommand.cs
+++ VoiceScript/VoiceScript/DiagramModel/Commands/EditCommand.cs
@@ -8,30 +8,36 @@ namespace VoiceScript.DiagramModel.Commands
 
         protected override void ProcessCommand(CommandExecutionContext context)
         {
-            if (targetType == "name")
+            if (IsEditNameCommand()) ChangeEditedComponentName(context);
+            else EnterEditedComponentContext(context);
+        }
+
+        bool IsEditNameCommand() => targetType == "name";
+
+        void ChangeEditedComponentName(CommandExecutionContext context)
+        {
+            context.CurrentComponent.Name = targetValue;
+            context.CommandExecuted = true;
+        }
+
+        void EnterEditedComponentContext(CommandExecutionContext context)
+        {
+            while (context.CurrentComponent != null && !context.CommandExecuted)
             {
-                context.CurrentComponent.Name = targetValue;
-                context.CommandExecuted = true;
-            }
-            else
-            {
-                while (context.CurrentComponent != null && !context.CommandExecuted)
+                var childToEdit = context.CurrentComponent.FindChild(targetType, targetValue);
+
+                if (childToEdit != null)
                 {
-                    var childToEdit = context.CurrentComponent.FindChild(targetType, targetValue);
-
-                    if (childToEdit != null)
-                    {
-                        context.TargetComponent = childToEdit;
-                        context.CommandExecuted = true;
-                    }
-                    else
-                    {
-                        context.CurrentComponent = context.CurrentComponent.Parent;
-                    }
+                    context.TargetComponent = childToEdit;
+                    context.CommandExecuted = true;
                 }
-
-                throw new InvalidOperationException("Component can not be edited. It does not exist in the current context.");
+                else
+                {
+                    context.CurrentComponent = context.CurrentComponent.Parent;
+                }
             }
+
+            throw new InvalidOperationException("Component can not be edited. It does not exist in the current context.");
         }
     }
 }
