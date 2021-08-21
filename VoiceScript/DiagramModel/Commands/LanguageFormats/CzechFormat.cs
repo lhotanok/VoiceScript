99a61bc91346544d84df452d33c05d163b3b29f5
Kristýna Lhoťanová <kristyna.posta@seznam.cz>
2021-08-21 23:34:26 +0200

    Add alternative component names

--- VoiceScript/DiagramModel/Commands/LanguageFormats/CzechFormat.cs
+++ VoiceScript/DiagramModel/Commands/LanguageFormats/CzechFormat.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+
+using VoiceScript.DiagramModel.Components;
 
 namespace VoiceScript.DiagramModel.Commands.LanguageFormats
 {
@@ -15,9 +16,22 @@ namespace VoiceScript.DiagramModel.Commands.LanguageFormats
             { DeleteCommand.DefaultFormat, new() { "smaž", "vymaž", "odstraň", "vystřihni" } }
         };
 
+        static readonly Dictionary<string, string> componentNames = new()
+        {
+            { Diagram.TypeName, "diagram" },
+            { Class.TypeName, "třídu"},
+            { Field.TypeName, "člena" },
+            { Method.TypeName, "metodu" },
+            { Type.TypeName, "typ" },
+            { Parameter.TypeName, "parametr" },
+            { Required.TypeName, "povinnost" },
+            { Visibility.TypeName, "viditelnost" }
+        };
+
         static readonly string delimiterFormat = "přepni";
 
         public override Dictionary<string, List<string>> CommandFormats { get => commandFormats; }
+        public override Dictionary<string, string> ComponentNames => componentNames;
         public override string DelimiterFormat => delimiterFormat;
     }
 }
--- VoiceScript/DiagramModel/Commands/LanguageFormats/Format.cs
+++ VoiceScript/DiagramModel/Commands/LanguageFormats/Format.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+
+using VoiceScript.DiagramModel.Components;
 
 namespace VoiceScript.DiagramModel.Commands.LanguageFormats
 {
     abstract class Format
     {
+        Dictionary<string, string> componentNames = new()
+        {
+            { Diagram.TypeName, Diagram.TypeName },
+            { Class.TypeName, Class.TypeName },
+            { Field.TypeName, Field.TypeName },
+            { Method.TypeName, Method.TypeName },
+            { Type.TypeName, Type.TypeName },
+            { Parameter.TypeName, Parameter.TypeName },
+            { Required.TypeName, Required.TypeName },
+            { Visibility.TypeName, Visibility.TypeName }
+        };
         public abstract Dictionary<string, List<string>> CommandFormats { get; }
+        public virtual Dictionary<string, string> ComponentNames { get => componentNames; }
         public abstract string DelimiterFormat { get; }
         /// <summary>
         /// Get list of possible command formats.
