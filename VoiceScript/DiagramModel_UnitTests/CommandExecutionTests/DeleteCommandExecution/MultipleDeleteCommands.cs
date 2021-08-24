feb13ea1984ed7dbd93a909f22aed452a666a0e2
Kristýna Lhoťanová <kristyna.posta@seznam.cz>
2021-08-24 23:04:54 +0200

    Add unit tests for DeleteCommand

---
+++ VoiceScript/DiagramModel_UnitTests/CommandExecutionTests/DeleteCommandExecution/MultipleDeleteCommands.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DiagramModel_UnitTests.CommandExecutionTests
+{
+    [TestFixture]
+    public class MultipleDeleteCommands : CommandExecution
+    {
+        [TestCase("add class person add class employee add class student delete class employee delete class student")]
+        [TestCase("add class person edit class person add class student delete class student")]
+        public void ExecuteDeleteCommands_CheckThatClassesWereDeleted(string inputText)
+        {
+            diagram.ConvertTextToDiagram(inputText);
+
+            Assert.AreEqual(1, diagram.GetClasses().Count);
+        }
+
+        [TestCase("add class person add field name add method get name delete field name delete method get name")]
+        [TestCase("add class person add field name add type string add method get name add type string delete field name delete method get name")]
+        public void ExecuteDeleteCommands_CheckThatParentContextRemained(string inputText)
+        {
+            ExecuteCommandsManually(inputText);
+
+            Assert.AreEqual(context.TargetComponent, diagram.GetClasses()[0]);
+        }
+
+        [TestCase("add class person add field name add type string add method get name add visibility public delete field name delete visibility public")]
+        public void TryExecuteDeleteCommandsWithInvalidTargetTypeInCurrentContext_CheckThatExceptionIsThrown(string inputText)
+        {
+            Assert.Throws<InvalidOperationException>(() => diagram.ConvertTextToDiagram(inputText));
+        }
+    }
+}
